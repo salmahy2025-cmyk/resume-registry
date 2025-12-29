cbaa1fce0e5162309e298c17f30299a4a31441cd
salmahy2025-cmyk <salma.hy2025@gmail.com>
2025-12-29 22:49:06 +0100

    Create util.php

---
+++ util.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+
+function flashMessages() {
+    if ( isset($_SESSION['error']) ) {
+        echo '<p style="color:red">'.htmlentities($_SESSION['error'])."</p>\n";
+        unset($_SESSION['error']);
+    }
+    if ( isset($_SESSION['success']) ) {
+        echo '<p style="color:green">'.htmlentities($_SESSION['success'])."</p>\n";
+        unset($_SESSION['success']);
+    }
+}
+
+function checkLoggedIn() {
+    if (!isset($_SESSION['user_id'])) {
+        die("Non connecté");
+    }
+}
