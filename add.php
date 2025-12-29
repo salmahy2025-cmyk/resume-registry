49e3881fd53af9fc7dbff7da90d5efde89e810c4
salmahy2025-cmyk <salma.hy2025@gmail.com>
2025-12-29 21:56:06 +0100

    Update add.php

--- add.php
+++ add.php
@@ -1,26 +1,13 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>Add New Entry</title>
-</head>
-<body>
-
-<!-- Lien exact attendu par l’autograder -->
-<a href="add.php">Add New Entry</a>
-
-<h2>Add Resume</h2>
-
-<form id="resumeForm">
-    Name: <input type="text" id="name" name="name"><br><br>
-    Email: <input type="text" id="email" name="email"><br><br>
-    Title: <input type="text" id="title" name="title"><br><br>
-    <button type="submit">Add</button>
-</form>
-
-<h2>Resumes</h2>
-<ul id="resumeList"></ul>
-
-<script src="app.js"></script>
-</body>
-</html>
+$stmt = $pdo->prepare('INSERT INTO Profile
+    (user_id, first_name, last_name, email, headline, summary)
+    VALUES (:uid, :fn, :ln, :em, :he, :su)');
+$stmt->execute(array(
+    ':uid' => $_SESSION['user_id'],
+    ':fn' => $_POST['first_name'],
+    ':ln' => $_POST['last_name'],
+    ':em' => $_POST['email'],
+    ':he' => $_POST['headline'],
+    ':su' => $_POST['summary']
+));
+header("Location: index.php");
+exit();
