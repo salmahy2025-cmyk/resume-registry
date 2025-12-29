ed22fef637c6deea1547d330c5135b9e90cbc780
salmahy2025-cmyk <salma.hy2025@gmail.com>
2025-12-29 22:44:48 +0100

    Delete pdo.php

--- pdo.php
+++
@@ -1,5 +0,0 @@
-<?php
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=votre_base_de_donnees', 
-               'votre_utilisateur', 'votre_mot_de_passe');
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
