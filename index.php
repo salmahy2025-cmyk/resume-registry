43cce35259fe48001fc483fb50d25ffa7a0a20d6
salmahy2025-cmyk <salma.hy2025@gmail.com>
2025-12-29 19:39:38 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Login - 79f3577a</title>
+</head>
+<body>
+
+<a href="#">Please log in</a> <!-- texte exact requis -->
+
+<h2>Login</h2>
+
+<form method="post" action="login.php">
+    Email: <input type="email" name="email" required><br><br>
+    Password: <input type="password" name="pass" required><br><br>
+    <button type="submit">Log In</button>
+</form>
+
+</body>
+</html>
