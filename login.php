0fd546e32953f5469518bce554ab525d0438ccef
salmahy2025-cmyk <salma.hy2025@gmail.com>
2025-12-30 22:29:15 +0100

    Delete login.php

--- login.php
+++
@@ -1,6 +0,0 @@
-<script src="app.js"></script>
-<form method="post">
-Email: <input type="text" name="email" id="id_email"><br>
-Password: <input type="password" name="pass" id="id_pass"><br>
-<input type="submit" onclick="return validateLogin();" value="Log In">
-</form>
